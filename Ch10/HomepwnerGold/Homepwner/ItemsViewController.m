6070ed31e66ca3e13803816337b2c5b0c9de9ad7
Adrian Istrate <adr_istrate@yahoo.com>
2013-10-29 18:31:43 +0100

    Chapter 10 - Silver and Gold challenges (one project).

---
+++ Ch10/HomepwnerGold/Homepwner/ItemsViewController.m
@@ -0,0 +1,171 @@
+//
+//  ItemsViewController.m
+//  Homepwner
+//
+//  Created by Adrian Istrate on 28/10/2013.
+//  Copyright (c) 2013 Adrian Istrate. All rights reserved.
+//
+
+#import "ItemsViewController.h"
+#import "BNRItemStore.h"
+#import "BNRItem.h"
+
+@implementation ItemsViewController
+
+- (id)init
+{
+    self = [super initWithStyle:UITableViewStyleGrouped];
+    if (self) {
+        for (int i = 0; i < 5; i++) {
+            [[BNRItemStore sharedStore] createItem];
+        }
+    }
+    return self;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    return [self init];
+}
+
+- (UIView *)headerView
+{
+    // If we haven't loaded the headerView yet...
+    if (!headerView) {
+        // Load HeaderView.xib
+        [[NSBundle mainBundle] loadNibNamed:@"HeaderView" owner:self options:nil];
+    }
+    
+    return headerView;
+}
+
+- (UIView *)tableView:(UITableView *)tv viewForHeaderInSection:(NSInteger)sec
+{
+    return [self headerView];
+}
+
+- (CGFloat)tableView:(UITableView *)tv heightForHeaderInSection:(NSInteger)sec
+{
+    // The height of the header view should be determined from the height of the
+    // view in the XIB file
+    return [[self headerView] bounds].size.height;
+}
+
+- (IBAction)toggleEditingMode:(id)sender
+{
+    // If we are currently in editing mode...
+    if ([self isEditing]) {
+        // Change text of button to inform user of state
+        [sender setTitle:@"Edit" forState:UIControlStateNormal];
+        // Turn off editing mode
+        [self setEditing:NO animated:YES];
+    } else {
+        // Change text of button to inform user of state
+        [sender setTitle:@"Done" forState:UIControlStateNormal];
+        // Enter editing mode
+        [self setEditing:YES animated:YES];
+    }
+}
+
+- (IBAction)addNewItem:(id)sender
+{
+    // Create a new BNRItem and add it to the store
+    BNRItem *newItem = [[BNRItemStore sharedStore] createItem];
+    
+    // Figure out where that item is in the array
+    int lastRow = [[[BNRItemStore sharedStore] allItems] indexOfObject:newItem];
+    
+    NSIndexPath *ip = [NSIndexPath indexPathForRow:lastRow inSection:0];
+    
+    // Insert this new row into the table.
+    [[self tableView] insertRowsAtIndexPaths:[NSArray arrayWithObject:ip]
+                            withRowAnimation:UITableViewRowAnimationTop];
+}
+
+-  (void)tableView:(UITableView *)tableView
+commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+ forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // If the table view is asking to commit a delete command...
+    if (editingStyle == UITableViewCellEditingStyleDelete)
+    {
+        BNRItemStore *ps = [BNRItemStore sharedStore];
+        NSArray *items = [ps allItems];
+        BNRItem *p = [items objectAtIndex:[indexPath row]];
+        [ps removeItem:p];
+        
+        // We also remove that row from the table view with an animation
+        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                         withRowAnimation:UITableViewRowAnimationFade];
+    }
+}
+
+-               (NSIndexPath *)tableView:(UITableView *)tableView
+targetIndexPathForMoveFromRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+                     toProposedIndexPath:(NSIndexPath *)proposedDestinationIndexPath
+{
+    NSUInteger itemCount = [[[BNRItemStore sharedStore] allItems] count];
+    
+    if ([sourceIndexPath row] == itemCount)
+        return sourceIndexPath;
+    else if ([proposedDestinationIndexPath row] == itemCount)
+        return [NSIndexPath indexPathForRow:itemCount - 1 inSection:0];
+    else
+        return proposedDestinationIndexPath;
+}
+
+-  (void)tableView:(UITableView *)tableView
+moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath
+       toIndexPath:(NSIndexPath *)destinationIndexPath
+{
+    [[BNRItemStore sharedStore] moveItemAtIndex:[sourceIndexPath row]
+                                        toIndex:[destinationIndexPath row]];
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView
+           editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if ([indexPath row] < [[[BNRItemStore sharedStore] allItems] count])
+        return UITableViewCellEditingStyleDelete;
+    else
+        return UITableViewCellEditingStyleNone;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView
+ numberOfRowsInSection:(NSInteger)section
+{
+    return [[[BNRItemStore sharedStore] allItems] count] + 1;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView
+         cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = nil;
+    
+    if ([indexPath row] < [[[BNRItemStore sharedStore] allItems] count]) {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell"];
+        
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                          reuseIdentifier:@"UITableViewCell"];
+        }
+        
+        BNRItem *p = [[[BNRItemStore sharedStore] allItems]
+                      objectAtIndex:[indexPath row]];
+        
+        [[cell textLabel] setText:[p description]];
+    } else {
+        cell = [tableView dequeueReusableCellWithIdentifier:@"UITableViewCell-Last"];
+        
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
+                                          reuseIdentifier:@"UITableViewCell-Last"];
+            
+            [[cell textLabel] setText:@"No more items!"];
+        }
+    }
+    
+    return cell;
+}
+
+@end
